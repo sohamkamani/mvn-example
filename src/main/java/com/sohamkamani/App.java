862617cf0ae3a0003f4690a26daf1285169992c1
sohamkamani <sohamkamani@gmail.com>
2021-07-18 12:27:41 +0530

    init

---
+++ src/main/java/com/sohamkamani/App.java
@@ -0,0 +1,17 @@
+package com.sohamkamani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int add(int n1, int n2){
+        return n1 + n2;
+    }
+}
---
+++ src/test/java/com/sohamkamani/AppTest.java
@@ -0,0 +1,17 @@
+package com.sohamkamani;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        int result = App.add(3, 4);
+        assertEquals(7, result);
+    }
+}
