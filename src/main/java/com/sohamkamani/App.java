f81a1d94dcdb9b9363d35215910f237eb1acaf77
sohamkamani <sohamkamani@gmail.com>
2021-11-03 15:39:44 +0530

    added assembly plugin

--- src/main/java/com/sohamkamani/App.java
+++ src/main/java/com/sohamkamani/App.java
@@ -1,12 +1,17 @@
 package com.sohamkamani;
 
+import com.github.ricksbrown.cowsay.Cowsay;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.printf("Hello World! %d", Calculator.add(4, 5));
+        int result = Calculator.add(4, 5);
+        String[] cowArgs = new String[] { String.valueOf(result) };
+        String cowString = Cowsay.say(cowArgs);
+        System.out.printf(cowString);
     }
 
     public static int add(int n1, int n2) {
