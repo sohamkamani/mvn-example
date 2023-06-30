0d50d0607e2aad3af87e1f13d392a4d8489d700a
sohamkamani <sohamkamani@gmail.com>
2023-06-30 13:27:54 +0530

    add argument parser

--- src/main/java/com/sohamkamani/App.java
+++ src/main/java/com/sohamkamani/App.java
@@ -12,7 +12,21 @@ public class App {
         String[] cowArgs = new String[] {String.valueOf(result)};
         String cowString = Cowsay.say(cowArgs);
         System.out.printf(cowString);
-        System.out.println(String.join(" | ", args) + args.length);
+    }
+
+    static void printCommandLineArgs(String[] args) {
+        // first, we should check if the user has provided the correct number of arguments
+        if (args.length != 2) {
+            // if not, we should print a usage message and exit
+            System.out.println(
+                    "Usage: java -jar target/cowsay-1.0-SNAPSHOT.jar \"quote\" \"author\"");
+            System.exit(1);
+        }
+        String quote = args[0];
+        String author = args[1];
+        String[] cowArgs = new String[] {quote, " --" + author};
+        String cowString = Cowsay.say(cowArgs);
+        System.out.printf(cowString);
     }
 
 }
