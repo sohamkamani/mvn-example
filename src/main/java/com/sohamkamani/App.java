717562b2e4cf897ae3cd493213b1179f1f5031db
sohamkamani <sohamkamani@gmail.com>
2023-06-30 13:22:51 +0530

    remove unused method

--- src/main/java/com/sohamkamani/App.java
+++ src/main/java/com/sohamkamani/App.java
@@ -9,12 +9,10 @@ import com.github.ricksbrown.cowsay.Cowsay;
 public class App {
     public static void main(String[] args) {
         int result = Calculator.add(4, 5);
-        String[] cowArgs = new String[] { String.valueOf(result) };
+        String[] cowArgs = new String[] {String.valueOf(result)};
         String cowString = Cowsay.say(cowArgs);
         System.out.printf(cowString);
+        System.out.println(String.join(" | ", args) + args.length);
     }
 
-    public static int add(int n1, int n2) {
-        return n1 + n2;
-    }
 }
